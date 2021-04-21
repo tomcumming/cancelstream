a6215484ba732432fb9ecd2221a95d26dbf87358
Tom Cumming <mail@tomcumming.co.uk>
2021-04-21 19:38:25 +0100

    Name cancel trigger

--- cancel.ts
+++ cancel.ts
@@ -3,10 +3,12 @@ export const CANCELLED = Symbol("Cancelled");
 // This can't be a promise because typescript will flatten it
 export type CancelSignal = [Promise<typeof CANCELLED>];
 
+export type CancelTrigger = () => void;
+
 export function cancelSignal(
   parent?: CancelSignal
-): { cs: CancelSignal; cancel: () => void } {
-  let cancel: () => void = undefined as any;
+): { cs: CancelSignal; cancel: CancelTrigger } {
+  let cancel: CancelTrigger = undefined as any;
   const cp: CancelSignal[0] = new Promise((res) => {
     cancel = () => res(CANCELLED);
   });
